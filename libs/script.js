339c4dda535d408a2938cb4103bf7a8886780257
Filipe Pires Guimaraes <filipepires@MacBook-Pro-Retina-de-Filipe.local>
2017-09-19 02:26:25 -0300

    base pronta

---
+++ libs/script.js
@@ -0,0 +1,15 @@
+function openGit() {
+    window.open('http://www.github.com/filipepiresg')
+}
+
+function openInsta() {
+    window.open('http://www.instagram.com/filipepiresg')
+}
+
+function openLattes() {
+    window.open('http://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K4404958P5')
+}
+
+function mailto() {
+    window.location.href = "mailto:liipepires@gmail.com";
+}
